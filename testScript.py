1a0b0df791c706426b5ae176caa051436e9dea59
мой_никнейм <_@mail.ru>
2025-11-19 18:22:18 +0600

    Add divMod function and update testScript

--- testScript.py
+++ testScript.py
@@ -1,12 +1,1 @@
-def greet(name):
-    print("Привет, " + name + "!")
-
-def add(a, b):
-    return a + b
-
-# Вызов функции приветствия
-greet("Дмитрий")
-
-# Сложение двух чисел и вывод результата
-result = add(3, 5)
-print("Сумма: ", result)
+print("У меня  получилось отправить файлы на GitHub через PyCharm! Урра!!")
